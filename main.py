75b7b651d4fbf63f4ec0257034ee8c767a3fb613
TYChan6028 <F04066028@gs.ncku.edu.tw>
2020-12-10 17:41:41 +0800

    Add elapsed time prompt to solution() function call

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-from solution import solution
+from __future__ import print_function
+import solution as s1
+import solution2 as s2
+import timeit
 
 # foobar accepts brilliant solution up until n = 5000
 
 n = 10000
-print(solution(n))
+
+start = timeit.default_timer()
+print(s1.solution(n))
+stop = timeit.default_timer()
+print('Time for s1: ', round(stop - start, 4), 'secs')
