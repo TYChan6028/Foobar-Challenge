319a23346169d787147b4616600f2a2b7dd9da64
TYChan6028 <F04066028@gs.ncku.edu.tw>
2020-12-10 17:44:03 +0800

    Update solution to version 2.0
    [Method] divide by all known prime numbers
    Verifying solution...
    All test cases passed. Use submit solution.py to submit your solution

--- solution.py
+++ solution.py
@@ -1,30 +1,24 @@
 import math
 
-
 # prime numbers up to 20219
+
+
 def solution(n):
-    num = 100
-    List = []
-
-    for x in xrange(2, num):  # add numbers from 2 to num - 1 to List
-        List.append(x)
-    # note the above statement is equivalent to List = range(2, num)
-
-    p = 2  # p is a prime
-
-    # continue to mark out primes until square root of p is less than num
-    while not int(math.sqrt(p)) + 1 > num:
-        # remove all the multiples of p
-        for x in xrange(p * 2, num, p):
-            List[x - 2] = 0
-        p += 1
-        # assign p to the next integer. Next prime is the next non zero number in the list
-        while p - 2 < len(List) and List[p - 2] == 0:
-            p += 1
+    ct = 2
+    max_ = 30000
+    p_list = [2, 3]
+    for x in xrange(5, max_, 2):
+        for p in p_list:
+            if x % p == 0:
+                break
+        else:
+            p_list.append(x)
+            ct += int(math.log10(x)) + 1
+        if ct >= n + 5:
+            break
 
     s = ''
-    for x in List:
-        if x != 0:
-            s = s + str(x)
+    for p in p_list:
+        s += str(p)
 
     return s[n:n + 5]
