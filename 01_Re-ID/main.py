0b755e2b5222aae9907fca53848338624d21c25f
TYChan6028 <F04066028@gs.ncku.edu.tw>
2020-12-11 15:48:19 +0800

    Print out peak memory usage

--- 01_Re-ID/main.py
+++ 01_Re-ID/main.py
@@ -1,13 +1,14 @@
 from __future__ import print_function
-import solution as s1
-import solution2 as s2
+from solution import solution
 import timeit
+import resource
 
 # foobar accepts brilliant solution up until n = 5000
 
 n = 10000
 
 start = timeit.default_timer()
-print(s1.solution(n))
+print(solution(n))
 stop = timeit.default_timer()
-print('Time for s1: ', round(stop - start, 4), 'secs')
+print('Time for solution():', round(stop - start, 4), 'secs')
+print('Peak memory usage:', resource.getrusage(resource.RUSAGE_SELF).ru_maxrss / 1024, 'KBs')
